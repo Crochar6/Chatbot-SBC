48b9e06a212b0d5f6253a38438e72fb0498301d2
Carles <carles.roch@onlean.com>
2022-02-25 13:38:42 +0100

    Fix write file closing and add genres

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import itertools
 import pandas as pd
 import ast
 
-NUM_READ = 4  # Número de línies a llegir del fitxer
+NUM_READ = 5  # Número de línies a llegir del fitxer
 
 
 if __name__ == "__main__":
-    outfile = open("parsed_data.csv", 'w', encoding="utf8")
+
+    output_data = ""
     outfile_header = "title,language,genres,summary\n"  # Header ha de coincidir amb les dades
-    outfile.write(outfile_header)
+    output_data += outfile_header
 
     with open("datasets/movies_metadata.csv", 'r', encoding="utf8") as infile:
         reader = csv.reader(infile, delimiter=",")
@@ -19,9 +20,14 @@ if __name__ == "__main__":
             # Agafar les dades que volem
             title = row[header.index("original_title")]
             language = row[header.index("original_language")]
-            genres = row[header.index("genres")]
+            genres_unparsed = row[header.index("genres")]
             summary = row[header.index("overview")]
 
+            genres_list = [language.get("name") for language in ast.literal_eval(genres_unparsed)]
+
             # Escriure al fitxer
-            line = f"{title},{language},{genres},{summary}\n"
-            outfile.write(line)
+            line = f"{title},{language},\"{genres_list}\",\"{summary}\"\n"
+            output_data += line
+
+    with open("parsed_data.csv", 'w', encoding="utf8") as outfile:
+        outfile.write(output_data)
