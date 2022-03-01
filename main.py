8d38d7dcaadb0ec97e838ef839ea11112d473884
Carles <carles.roch@onlean.com>
2022-03-01 19:28:58 +0100

    Migrate to using pandas library

--- main.py
+++ main.py
@@ -1,33 +1,9 @@
-import csv
-import itertools
 import pandas as pd
-import ast
-
-NUM_READ = 5  # Número de línies a llegir del fitxer
-
 
 if __name__ == "__main__":
+    metadata = pd.read_csv('datasets/movies_metadata.csv', low_memory=False, header=0)
+    metadata['id'] = metadata['id'].astype(str)
+    keywords = pd.read_csv('datasets/keywords.csv', header=0)
+    keywords['id'] = keywords['id'].astype(str)
 
-    output_data = ""
-    outfile_header = "title,language,genres,summary\n"  # Header ha de coincidir amb les dades
-    output_data += outfile_header
-
-    with open("datasets/movies_metadata.csv", 'r', encoding="utf8") as infile:
-        reader = csv.reader(infile, delimiter=",")
-        header = next(reader)
-        for row in itertools.islice(reader, NUM_READ):
-
-            # Agafar les dades que volem
-            title = row[header.index("original_title")]
-            language = row[header.index("original_language")]
-            genres_unparsed = row[header.index("genres")]
-            summary = row[header.index("overview")]
-
-            genres_list = [language.get("name") for language in ast.literal_eval(genres_unparsed)]
-
-            # Escriure al fitxer
-            line = f"{title},{language},\"{genres_list}\",\"{summary}\"\n"
-            output_data += line
-
-    with open("parsed_data.csv", 'w', encoding="utf8") as outfile:
-        outfile.write(output_data)
+    pd.merge(metadata, keywords, on='id', how='inner')
