6b04fd73a2d53cdafcd7f0f7f0bd482cb3a49756
Carles <carles.roch@onlean.com>
2022-03-07 12:41:56 +0100

    Import and merge both csv

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import pandas as pd
 
-if __name__ == "__main__":
-    metadata = pd.read_csv('datasets/movies_metadata.csv', low_memory=False, header=0)
+
+def import_raw():
+    metadata = pd.read_csv('datasets/movies_metadata.csv',
+                           usecols=['id', 'title', 'genres', 'vote_average', 'vote_count', 'original_language',
+                                    'runtime', 'release_date', 'overview'],
+                           header=0)
     metadata['id'] = metadata['id'].astype(str)
     keywords = pd.read_csv('datasets/keywords.csv', header=0)
     keywords['id'] = keywords['id'].astype(str)
 
-    pd.merge(metadata, keywords, on='id', how='inner')
+    df = pd.merge(metadata, keywords, on='id', how='inner')
+
+    return df
+
+
+if __name__ == "__main__":
+    database = import_raw()
+    print(database.info())
