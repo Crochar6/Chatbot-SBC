36611760f929d116e4916670dce5daefa6c1dc31
Carles <carles.roch@onlean.com>
2022-03-07 13:03:34 +0100

    Change type of lists

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pandas as pd
+import ast
 
 
 def import_raw():
     metadata = pd.read_csv('datasets/movies_metadata.csv',
                            usecols=['id', 'title', 'genres', 'vote_average', 'vote_count', 'original_language',
                                     'runtime', 'release_date', 'overview'],
-                           header=0)
+                           header=0,
+                           converters={3: ast.literal_eval})    # Reformat String -> List of Dictionaries
     metadata['id'] = metadata['id'].astype(str)
-    keywords = pd.read_csv('datasets/keywords.csv', header=0)
+    keywords = pd.read_csv('datasets/keywords.csv',
+                           header=0,
+                           converters={1: ast.literal_eval})    # Reformat String -> List of Dictionaries
     keywords['id'] = keywords['id'].astype(str)
 
     df = pd.merge(metadata, keywords, on='id', how='inner')
