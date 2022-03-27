56fb2a6e16d426689f3599ab092feb25ee01124a
Carles <roch.carles@gmail.com>
2022-03-27 19:58:18 +0200

    Add data ponderation constants

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import re
 import json
 from os.path import exists
 import bot
+import constant as const
 
 
 def tokenize(message):
@@ -277,10 +278,10 @@ if __name__ == "__main__":
         user_msg = tokenize(user_msg)
         msg_genres, msg_keywords = identify_genre(keywords, user_msg)
         msg_names = identify_persons(persons, user_msg)
-        punctuate_persons(database, msg_names, 1.3)
-        punctuate_genres(database, msg_genres, 1)
-        punctuate_keywords(database, msg_keywords, 0.8)
-        print(f'Genres: {msg_genres}, Keywords: {msg_keywords}, Person names: {msg_names}')
+        punctuate_persons(database, msg_names, const.PERSON_WEIGHT)
+        punctuate_genres(database, msg_genres, const.GENRE_WEIGHT)
+        punctuate_keywords(database, msg_keywords, const.KEYWORD_WEIGHT)
+        # print(f'Genres: {msg_genres}, Keywords: {msg_keywords}, Person names: {msg_names}')
         bot_answer, should_end = bot.calculate_response(user_msg, msg_genres, msg_keywords, msg_names, [])  # TODO: buscar noms de pel·lis
         print(bot_answer)
         if should_end:
